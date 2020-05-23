a3de46f20410b257038cbb20938976caf0c397df
secuflag <secuflag@ticino.com>
2020-05-23 16:04:59 +0200

    GCC 10.1.1 20200523
     * binutils-2.34
     * gmp-6.2.0
     * mpfr-4.0.2
     * mpc-1.1.0
     * isl-0.22.1
     * glibc-2.31
     * linux 5.6.14

--- aarch64-linux-gnu/include/c++/10.1.1/aarch64-linux-gnu/bits/c++config.h
+++ aarch64-linux-gnu/include/c++/10.1.1/aarch64-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 10
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20200520
+#define __GLIBCXX__ 20200523
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
