b8e909a757301175c66822125285c2a2c4b6c555
secuflag <secuflag@ticino.com>
2020-06-09 18:59:56 +0200

    GCC 10.1.1 20200609
     * binutils-2.34
     * gmp-6.2.0
     * mpfr-4.0.2
     * mpc-1.1.0
     * isl-0.22.1
     * glibc-2.31
     * linux 5.6.17

--- aarch64-linux-gnu/include/c++/10.1.1/aarch64-linux-gnu/bits/c++config.h
+++ aarch64-linux-gnu/include/c++/10.1.1/aarch64-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 10
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20200602
+#define __GLIBCXX__ 20200609
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
--- aarch64-linux-gnu/include/linux/version.h
+++ aarch64-linux-gnu/include/linux/version.h
@@ -1,2 +1,2 @@
-#define LINUX_VERSION_CODE 329231
+#define LINUX_VERSION_CODE 329233
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
